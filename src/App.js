b9b97530682087e11ca6e9f9fb35033548254b55
Michael Vega <micha3lvega@gmail.com>
2020-04-29 09:51:47 -0500

    add bootstrap depency

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 function App() {
   return (
     <div className="App">
