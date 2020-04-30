04547b11a9067a679ad242a7e9de301e6cd59c8f
Michael Vega <micha3lvega@gmail.com>
2020-04-30 06:28:34 -0500

    fix bug header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Jumbotron, Container } from 'reactstrap';
 
 const Header = () => (
-    <Header>
+    <Fragment className="header">
         <Jumbotron fluid>
             <Container fluid>
                 <h1 className="display-3">React application</h1>
             </Container>
         </Jumbotron>
-    </Header>);
+    </Fragment>);
 
 export default Header;
