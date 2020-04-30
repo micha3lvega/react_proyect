abccf446a876c4755a2f936e3f5c4e9252f42fab
Michael Vega <micha3lvega@gmail.com>
2020-04-30 06:27:39 -0500

    create component curso

--- src/components/Curso.js
+++ src/components/Curso.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button, CardTitle, CardText, Col} from 'reactstrap';
+import {
+    Card, Button, CardHeader, CardBody, Col, CardImg, Badge, CardTitle, CardText
+} from 'reactstrap';
 
 
 const Curso = (props) => {
     return (
         <div className="col-4">
             <Col>
-                <Card body>
-                    <CardTitle>Special Title Treatment</CardTitle>
-                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                    <Button>Go somewhere</Button>
+                <Card className="text-center">
+                    <CardImg top width="100%" src={props.image} alt={props.title} />
+                    <CardBody>
+                        <CardTitle><h5>{props.title}</h5></CardTitle>
+                        <CardText>
+                            <Badge pill color="danger">$20.00</Badge>
+                        </CardText>
+                        <Button color="primary">{props.textButton !== undefined ? props.textButton : 'Ingresar'}</Button>
+                    </CardBody>
                 </Card>
             </Col>
         </div>
