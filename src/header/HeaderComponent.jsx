6083f299d266953eeef673edeb6d5e56c2118e22
Michael Vega <micha3lvega@gmail.com>
2020-05-30 01:11:20 -0500

    refactor names

---
+++ src/header/HeaderComponent.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../header/Header.css'
+
+import { Container } from 'reactstrap';
+
+const Header = () => (
+    <Fragment className="header">
+        <Container fluid>
+            <h1 className="display-3">React application</h1>
+            <hr/>
+        </Container>
+    </Fragment>);
+
+export default Header;
